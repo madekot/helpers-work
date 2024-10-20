20f30c9c1d563d70e3b19d10436d30979ee5caf8
Mikhail Golubtsov <mgolubcov@gmail.com>
2024-10-20 23:02:34 +0300

    test

--- src/pages/sheet-calculator.tsx
+++ src/pages/sheet-calculator.tsx
@@ -7,7 +7,6 @@ export default function SheetCalculatorPage() {
             <Head >
                 <meta name="theme-color" content="#000000" />
                 <meta name="apple-mobile-web-app-capable" content="yes" />
-                <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
                 <link rel="apple-touch-icon" href="/calculator-apple-touch-icon.png" />
             </Head>
             <SheetCalculator />
