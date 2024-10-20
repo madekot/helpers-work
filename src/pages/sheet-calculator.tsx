fec7859cdd55e22a627a068d4b22efe7408ed946
Mikhail Golubtsov <mgolubcov@gmail.com>
2024-10-20 23:06:08 +0300

    save

--- src/pages/sheet-calculator.tsx
+++ src/pages/sheet-calculator.tsx
@@ -5,8 +5,11 @@ export default function SheetCalculatorPage() {
     return (
         <div>
             <Head >
+                <link rel="manifest" href="/manifest-calculator.json" />
+                <link rel="icon" href="/calculator-android-chrome-512x512.png" />
                 <meta name="theme-color" content="#000000" />
                 <meta name="apple-mobile-web-app-capable" content="yes" />
+                <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
                 <link rel="apple-touch-icon" href="/calculator-apple-touch-icon.png" />
             </Head>
             <SheetCalculator />
