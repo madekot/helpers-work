87aed256d1ecd73859b532ea9fe27cf3a36c2c5d
Mikhail Golubtsov <mgolubcov@gmail.com>
2024-10-20 21:10:01 +0300

    feat: add icon chrome

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ru">
-      <Head />
+      <Head >
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/android-chrome-192x192.png" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <link rel="apple-touch-icon" href="/android-chrome-192x192.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
