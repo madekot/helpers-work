01931e8805660c3725ee8b3ab286a87c51985d34
Mikhail Golubtsov <mgolubcov@gmail.com>
2024-10-20 22:43:38 +0300

    feat: separate calculator application

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,14 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ru">
-      <Head >
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" href="/android-chrome-192x192.png" />
-        <meta name="theme-color" content="#000000" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <link rel="apple-touch-icon" href="/android-chrome-192x192.png" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
--- src/pages/sheet-calculator.tsx
+++ src/pages/sheet-calculator.tsx
@@ -1,8 +1,16 @@
 import SheetCalculator from '@/components/SheetCalculator/SheetCalculator';
+import Head from 'next/head';
 
 export default function SheetCalculatorPage() {
     return (
         <div>
+            <Head >
+                <link rel="manifest" href="/manifest-calculator.json" />
+                <meta name="theme-color" content="#000000" />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+                <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+                <link rel="apple-touch-icon" href="/calculator-apple-touch-icon.png" />
+            </Head>
             <SheetCalculator />
         </div>
     );
